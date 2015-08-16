8c9f0a4f7be2ccab03e36a852082408c48b797fd
Ростислав <extra0@mail.ru>
2015-08-16 17:53:43 +0300

    готова шапка и слайдер на главной

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(function(){
+
+	//слайдер на главной
+	$('.main__top-slider').bxSlider({
+		speed: 700,
+		easing: 'cubic-bezier(.72,.05,.39,.37)',
+		controls: true,
+		auto: true,
+		autoHover: true
+	});
+
+});
