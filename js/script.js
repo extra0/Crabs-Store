49fd128a2471c60c954c211b8432a5806eb26d06
Ростислав <extra0@mail.ru>
2015-08-18 00:59:47 +0300

    готовность главной 80%

--- js/script.js
+++ js/script.js
@@ -9,4 +9,22 @@ $(function(){
 		autoHover: true
 	});
 
+	// слайдер покупок
+	$('.medium-slider').bxSlider({
+		speed: 450,
+		slideWidth: 350,
+		slideMargin: 20,
+		minSlides: 3,
+		maxSlides: 3,
+		easing: 'cubic-bezier(.57, .03, 0, 1)',
+		pager: false,
+		controls: true,
+		auto: false
+	});
+
+	// меняем задний фон банеров
+	$('.banner__block').hover(function(){
+		$(this).find('.banner__bg').toggleClass('_hover');
+	});
+
 });
